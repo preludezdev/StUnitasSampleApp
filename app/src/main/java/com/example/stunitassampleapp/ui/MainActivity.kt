67b334683055c71de979d574fcdaea923d440927
preludezdev <preludez.dev@gmail.com>
2019-10-27 13:09:22 +0900

    데바 & 뷰모델 구현

--- app/src/main/java/com/example/stunitassampleapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/stunitassampleapp/ui/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.stunitassampleapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.stunitassampleapp.R
+import com.example.stunitassampleapp.databinding.ActivityMainBinding
+import com.example.stunitassampleapp.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by lazy {
+        MainViewModel()
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/example/stunitassampleapp/vm/MainViewModel.kt
+++ app/src/main/java/com/example/stunitassampleapp/vm/MainViewModel.kt
@@ -1,14 +1,41 @@
 package com.example.stunitassampleapp.vm
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.stunitassampleapp.network.RetrofitHelper
+import com.example.stunitassampleapp.network.vo.KakaoImageResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
-    private val _queryString = MutableLiveData<String>()
-    val queryString: LiveData<String> get() = _queryString
+    val queryString = MutableLiveData<String>()
 
     fun searchQuery() {
+        RetrofitHelper
+            .getInstance()
+            .apiService
+            .searchImagesByQuery(API_KEY, queryString.value ?: "")
+            .enqueue(object : Callback<KakaoImageResponse> {
+                override fun onFailure(call: Call<KakaoImageResponse>, t: Throwable) {
+                    Log.d("test", "fail to connect kakao Api")
+                }
 
+                override fun onResponse(
+                    call: Call<KakaoImageResponse>,
+                    response: Response<KakaoImageResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d("test", "success")
+                    } else {
+                        Log.d("test", "${response.code()} ${response.message()}")
+                    }
+                }
+            })
+    }
+
+    companion object {
+        const val API_KEY = "KakaoAK 53cccb2e09fc1dfeec6ec2755b3e9325"
     }
 }
